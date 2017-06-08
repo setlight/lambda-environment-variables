e72301f2579f5236e363931e1747b2cd65dae69c
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-06-08 10:11:50 -0300

    serverless: add lambda configuration file

---
+++ handler.js
@@ -0,0 +1,11 @@
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Ok!'
+      method: process.env.method,
+    }),
+  }
+
+  callback(null, response)
+}
